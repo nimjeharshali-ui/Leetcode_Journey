c7c656c2e4819fcc2d3eaeda48a8c0c0f552de96
Harshali Nimje <nimjeharshali@gmail.com>
2026-08-21 23:25:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (75.27%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        for(long i = 1;i<=n;i=i*3){
+            if(n==i){
+                return true;
+            }
+          
+        }
+          return false;
+    }
+};
